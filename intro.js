ea7507ce46822fcd7b608dfc25d682c2902e8f29
muhamadKelvin1 <mkelvin2033@gmail.com>
2024-12-08 21:39:29 +0700

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,11 @@
+// Mengetahui nodeJS dasar +++...
+// -> // <-ini adalah comment agar tag/element tak kebaca oleh system
+
+var // variabel var dapat diperbaharui dan dideklarasikan ulang didalam scope nya
+let // variabel let dapat diperbaharui namun tidak dapat dideklarasikan ulang.
+const // Const tidak dapat di-redeklrasikan atau di-update, kecuali jika tipe data variabelnya berupa
+
+const variabel1 = 12345 // ini adalah tipe integer
+const variabel2 = "12345" // ini tipe string
+
+const 
